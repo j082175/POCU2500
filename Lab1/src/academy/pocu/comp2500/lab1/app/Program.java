c1716039bbbe6b5f1114a1503f1642ede2501208
JUNSOOCHO <j082175@naver.com>
2022-05-02 10:42:15 +0900

    2

--- Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
+++ Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
@@ -3,7 +3,9 @@ package academy.pocu.comp2500.lab1.app;
 public class Program {
 
     public static void main(String[] args) {
-       
+        Lab1 lb = new Lab1();
+       int result = lb.add(3, 3);
+       System.out.println(result);
         // Write your code here
     }
 }
