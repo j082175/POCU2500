3d40bba0cb978c445977f04d38e0d79768834a5b
JUNSOOCHO <j082175@naver.com>
2022-05-09 10:46:19 +0900

    lab2_1

---
+++ Lab1/src/academy/pocu/comp2500/lab1/academy/pocu/comp2500/lab1/Program.java
@@ -0,0 +1,12 @@
+package academy.pocu.comp2500.lab1;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Lab1 lb = new Lab1();
+        int result = lb.add(3, 3);
+        System.out.println(result);
+        assert(false);
+        // Write your code here
+    }
+}
---
+++ Lab2/src/academy/pocu/comp2500/lab2/app/academy/pocu/comp2500/lab2/app/ComplexNumber.java
@@ -0,0 +1,5 @@
+package academy.pocu.comp2500.lab2.app;
+
+public class ComplexNumber {
+    
+}
