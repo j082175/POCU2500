e0cf0d46a9cadab361412df3141d9f8fcc9c6a77
cho jun soo <j082175@naver.com>
2022-05-09 17:29:51 +0900

    1

--- Lab2/src/academy/pocu/comp2500/lab2/app/academy/pocu/comp2500/lab2/app/Program.java
+++ Lab2/src/academy/pocu/comp2500/lab2/app/academy/pocu/comp2500/lab2/app/Program.java
@@ -1,4 +1,6 @@
-package academy.pocu.comp2500.lab2;
+package academy.pocu.comp2500.lab2.app;
+
+import academy.pocu.comp2500.lab2.ComplexNumber;
 
 public class Program {
     private static final double DOUBLE_EPSILON = 0.00001;
