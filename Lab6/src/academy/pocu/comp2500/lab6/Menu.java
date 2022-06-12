2b2c0ae2286ffd9ddca12bc302bb9e27736001ee
JUNSOOCHO <j082175@naver.com>
2022-06-12 23:24:53 +0900

    f

--- Lab6/src/academy/pocu/comp2500/lab6/Menu.java
+++ Lab6/src/academy/pocu/comp2500/lab6/Menu.java
@@ -1,5 +1,20 @@
 package academy.pocu.comp2500.lab6;
 
+import java.util.ArrayList;
+
 public class Menu {
-    
+    protected int price;
+    protected ArrayList<Topping> toppings = new ArrayList<>();
+
+    public Menu(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public ArrayList<Topping> getToppings() {
+        return this.toppings;
+    }
 }
--- Lab6/src/academy/pocu/comp2500/lab6/app/Program.java
+++ Lab6/src/academy/pocu/comp2500/lab6/app/Program.java
@@ -1,8 +1,19 @@
 package academy.pocu.comp2500.lab6.app;
 
+import java.util.ArrayList;
+
+import academy.pocu.comp2500.lab6.MeatLoverPizza;
+import academy.pocu.comp2500.lab6.Topping;
+
 public class Program {
 
     public static void main(String[] args) {
-	    // write your code here
+        MeatLoverPizza meatLoverPizza = new MeatLoverPizza();
+        boolean isAdded = meatLoverPizza.addGreenPeppers(); // true
+        boolean isRemoved = meatLoverPizza.removeGreenPeppers(); // true
+        isAdded = meatLoverPizza.addRedOnions(); // true
+        boolean isValid = meatLoverPizza.isValid(); // true
+        int price = meatLoverPizza.getPrice(); // 21
+        ArrayList<Topping> toppings = meatLoverPizza.getToppings();
     }
 }
