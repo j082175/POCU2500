7063c13665e6894a227d26c2f1abb3bd4481ec82
j082175 <j082175@naver.com>
2022-07-15 10:16:02 +0900

    12

--- Lab9/src/academy/pocu/comp2500/lab9/BuyOneGetOneFree.java
+++ Lab9/src/academy/pocu/comp2500/lab9/BuyOneGetOneFree.java
@@ -1,10 +1,19 @@
 package academy.pocu.comp2500.lab9;
 
+import java.util.Collection;
 import java.util.HashSet;
 
 public class BuyOneGetOneFree {
 
     public BuyOneGetOneFree(HashSet skuNumber) {
 
+    }
+
+    public void getTotalPrice(Collection books) {
+
+    }
+
+    public void getTotalPrice(BuyOneGetOneFree buyOneGetOneFree) {
+        
     }
 }
