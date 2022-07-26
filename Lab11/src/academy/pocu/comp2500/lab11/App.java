bf6fc9037420e6b9779256f6f54b9fe9f55daa82
j082175 <j082175@naver.com>
2022-07-26 18:27:44 +0900

    4363

--- Lab11/src/academy/pocu/comp2500/lab11/App.java
+++ Lab11/src/academy/pocu/comp2500/lab11/App.java
@@ -1,5 +1,6 @@
 package academy.pocu.comp2500.lab11;
 
+
 import academy.pocu.comp2500.lab11.pocu.*;
 
 import java.io.BufferedReader;
--- Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
+++ Lab11/src/academy/pocu/comp2500/lab11/OverflowException.java
@@ -1,9 +1,9 @@
 package academy.pocu.comp2500.lab11;
 
 public class OverflowException extends RuntimeException {
-    private String message;
+    private static final long serialVersionUID = 20L;
 
     public OverflowException(String message) {
-        this.message = message;
+        super(message);
     }
 }
